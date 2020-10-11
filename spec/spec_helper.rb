dc4ffd4af20143bcd58f597a5420df4e0db2a963
Antonio <tofernandes67@gmail.com>
2020-10-11 19:03:17 +0100

    fix linter issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,19 +4,16 @@ RSpec.configure do |config|
   config.before(:all) do
     class HttParty
       include HTTParty
-      base_uri "#{Rails.application.credentials.api[:base_uri]}"
+      base_uri Rails.application.credentials.api[:base_uri].to_s
     end
   end
   config.expect_with :rspec do |expectations|
-    
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
-
     mocks.verify_partial_doubles = true
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
 end
