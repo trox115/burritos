7bbc573fb64a9f7698c168e9e9b80ab9a23ae71e
Antonio <tofernandes67@gmail.com>
2020-10-11 19:13:28 +0100

    create models to burrito db

---
+++ db/migrate/20201011181248_create_burritos_infos.rb
@@ -0,0 +1,19 @@
+class CreateBurritosInfos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :burritos_infos do |t|
+      t.string :name
+      t.string :address
+      t.decimal :lat
+      t.decimal :lng
+      t.integer :category_id
+      t.integer :category_id2
+      t.boolean :closed
+      t.string :hours
+      t.string :website
+      t.string :address_line_2
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
