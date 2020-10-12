e434f77bc7c93b4e5f42a032fe8b2190b5d30f70
Antonio <tofernandes67@gmail.com>
2020-10-12 11:32:34 +0100

    fix more linting

--- app/helpers/burritos_info_helper.rb
+++ app/helpers/burritos_info_helper.rb
@@ -24,11 +24,11 @@ module BurritosInfoHelper
   end
 
   def format_hours_for_a(platform, hours)
-    if platform === 'a' # rubocop:todo Style/CaseEquality
+    if platform.match?('a')
       new_hours = hours.gsub(/,/, '|')
       return new_hours.gsub(/ /, '-')
     end
-    if platform === 'b' # rubocop:todo Style/CaseEquality
+    if platform.match?('b')
       days_of_week = %w[Mon Tue Wed Thu Fri Sat Sun]
       arr = []
       hours = hours.split(',')
@@ -42,28 +42,28 @@ module BurritosInfoHelper
       return new_hours.gsub(/ /, '-')
     end
 
-    hours.gsub(/ /, '-') if platform === 'c' # rubocop:todo Style/CaseEquality
+    hours.gsub(/ /, '-') if platform.match?('c')
   end
 
   def format_for_platform(name, params)
-    if name === 'a' # rubocop:todo Style/CaseEquality
+    if name.match?('a')
       params[:address] = "#{params[:address]} #{params[:address2_line_2]}"
       return params
     end
-    if name === 'b' # rubocop:todo Style/CaseEquality
+    if name.match?('b')
       params[:street_address] = "#{params[:address]} #{params[:address2_line_2]}"
       params[:category_id] = params.delete :category_id2
       return params
     end
 
-    if name === 'c' # rubocop:todo Style/CaseEquality # rubocop:todo Style/GuardClause
+    if name.match?('b')
       params[:address_line_1] = params[:address]
       params
     end
   end
 
   def checkresponse(response)
-    if 200 === response.to_i # rubocop:todo Style/CaseEquality
+    if response.to_i.equal?(200)
       1
     else
       0
@@ -81,9 +81,9 @@ module BurritosInfoHelper
       platforms = %w[a b c]
       flag = 1
       platforms.each do |x|
-        flag = 0 if @api_service.update(x, JSON.parse(params)) === 1 # rubocop:todo Style/CaseEquality
+        flag = 0 if @api_service.update(x, JSON.parse(params)).equal?(1)
       end
     end
-    Stack.delete_all if flag === 1 # rubocop:todo Style/CaseEquality
+    Stack.delete_all if flag.equal?(1)
   end
 end
